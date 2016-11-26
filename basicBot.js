fc170369dba398171624e7459ea935b47d545b1c
dec0m <decom369@outlook.com>
2016-11-26 03:07:46 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1967,6 +1967,76 @@
                     }
                 }
             },
+             trustCommand: {
+                command: 'trust',
+                rank: 'user',
+                type: 'startsWith',
+                getTrust: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.trusts.length);
+                    return basicBot.chat.trusts[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.noTrust, {nameto: user.username, namefrom: chat.un}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfTrust, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.trust, {name: name, trust: this.getTrust()}));
+                            }
+                        }
+                    }
+                }
+            },
+             nosenCommand: {
+                command: 'cookie',
+                rank: 'user',
+                type: 'startsWith',
+                getCookie: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.cookies.length);
+                    return basicBot.chat.cookies[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
+                            }
+                        }
+                    }
+                }
+            },
 
             cycleCommand: {
                 command: 'cycle',
